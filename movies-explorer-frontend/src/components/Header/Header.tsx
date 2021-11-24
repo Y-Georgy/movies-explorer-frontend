2a66e5d64ffe6869a340baa76e7ec0c2a2698afc
Georgy Yadrihinskiy <y-georgy@yandex.ru>
2021-11-24 13:46:15 +0300

    refactor: header

--- movies-explorer-frontend/src/components/Header/Header.tsx
+++ movies-explorer-frontend/src/components/Header/Header.tsx
@@ -5,17 +5,17 @@ import logo from '../../images/logo.svg'
 function Header() {
   return (
     <header className="header">
-      <img src={logo} alt="Логотип Movies-explorer" className="header__logo" />
-      <nav className="header__links">
-        <div className="header__link-register">
-          Регистрация
-        </div>
-        <div className="header__link-login">
-          Войти
-        </div>
-      </nav>
-
-
+      <section className="header__container">
+        <img src={logo} alt="Логотип Movies-explorer" className="header__logo" />
+        <nav className="header__links">
+          <div className="header__link-register">
+            Регистрация
+          </div>
+          <div className="header__link-login">
+            Войти
+          </div>
+        </nav>
+      </section>
     </header>
   )
 }
