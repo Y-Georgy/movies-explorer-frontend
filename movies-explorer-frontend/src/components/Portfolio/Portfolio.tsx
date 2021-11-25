56904b6111251105c20cf67a2a860b0575ac1aed
Georgy Yadrihinskiy <y-georgy@yandex.ru>
2021-11-25 18:32:23 +0300

    feat: адаптивная верстка секции portfolio

---
+++ movies-explorer-frontend/src/components/Portfolio/Portfolio.tsx
@@ -0,0 +1,29 @@
+import './Portfolio.css'
+import { Link } from 'react-router-dom'
+
+function Portfolio() {
+  return (
+    <section className="portfolio main__section">
+      <h2 className="portfolio__title">Портфолио</h2>
+      <ul className="portfolio__list">
+        <li className="portfolio__list-elem">
+          <a href="https://ya.ru" className="portfolio__link" target="_blank" rel="noreferrer">
+            Статичный сайтa
+          </a>
+        </li>
+        <li className="portfolio__list-elem">
+          <a href="https://ya.ru" className="portfolio__link" target="_blank" rel="noreferrer">
+            Адаптивный сайт
+          </a>
+        </li>
+        <li className="portfolio__list-elem">
+          <a href="https://ya.ru" className="portfolio__link" target="_blank" rel="noreferrer">
+            Одностраничное приложение
+          </a>
+        </li>
+      </ul>
+    </section >
+  )
+}
+
+export default Portfolio;
