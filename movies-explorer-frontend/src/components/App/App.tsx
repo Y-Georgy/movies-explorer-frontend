12f2c3dffee621cf8b80fbdbfe69d39c8d26aa82
Georgy Yadrihinskiy <y-georgy@yandex.ru>
2021-11-25 18:31:12 +0300

    feat: встроил route

--- movies-explorer-frontend/src/components/App/App.tsx
+++ movies-explorer-frontend/src/components/App/App.tsx
@@ -1,15 +1,23 @@
 import React from 'react';
 import Header from '../Header/Header';
 import Main from '../Main/Main';
+import Movies from '../Movies/Movies';
 import './App.css';
+import { Route, Routes } from 'react-router-dom'
+
 
 function App() {
-  // let arr: string[] = ['Kate', 'Georgy'];
-
   return (
     <>
-      <Header />
-      <Main />
+      <Routes>
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/" element={
+          <>
+            <Header />
+            <Main />
+          </>
+        } />
+      </Routes>
     </>
   );
 }
