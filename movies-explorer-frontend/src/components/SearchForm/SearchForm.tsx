6ab5944ec707df5e054f4351100dad372226c48d
Georgy Yadrihinskiy <y-georgy@yandex.ru>
2021-11-29 16:05:06 +0300

    feat: адаптив к searchForm

--- movies-explorer-frontend/src/components/SearchForm/SearchForm.tsx
+++ movies-explorer-frontend/src/components/SearchForm/SearchForm.tsx
@@ -3,11 +3,12 @@ import FilterCheckbox from '../FilterCheckbox/FilterCheckbox';
 
 function SearchForm() {
   return (
-    <form method="GET" className="search-form" name="search">
-      <input className="search-form__query" placeholder="Фильм" type="text" required/>
-      <button type="submit" className="search-form__btn-submit">Поиск</button>
-      <FilterCheckbox />
-    </form>
+      <form method="GET" className="search-form" name="search">
+        <input className="search-form__query" placeholder="Фильм" type="text" required/>
+        <button type="submit" className="search-form__btn-submit">Поиск</button>
+        <FilterCheckbox />
+        <hr className="search-form__line"/>
+      </form>
   )
 }
 
