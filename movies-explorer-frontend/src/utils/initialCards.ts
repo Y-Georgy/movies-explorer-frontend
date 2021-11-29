0fe6c33e4e9eecf8acc0229905a92ceb41e798f6
Georgy Yadrihinskiy <y-georgy@yandex.ru>
2021-11-29 21:33:32 +0300

    feat: замоканные данные

---
+++ movies-explorer-frontend/src/utils/initialCards.ts
@@ -0,0 +1,53 @@
+interface ICard {
+  nameRU: string,
+  duration: number,
+  url: string
+}
+
+export const initialCards: ICard[] = [
+  {
+    nameRU: "Роллинг Стоунз» в изгнании",
+    duration: 61,
+    url: "https://api.nomoreparties.co/uploads/thumbnail_stones_in_exile_b2f1b8f4b7.jpeg"
+  },
+  {
+    nameRU: "All Tomorrow's Parties",
+    duration: 82,
+    url: "https://api.nomoreparties.co/uploads/thumbnail_all_tommoros_parties_33a125248d.jpeg"
+  },
+  {
+    nameRU: "Без обратного пути",
+    duration: 104,
+    url: "https://api.nomoreparties.co/uploads/thumbnail_blur_a43fcf463d.jpeg"
+  },
+  {
+    nameRU: "Фавела на взрыве",
+    duration: 80,
+    url: "https://api.nomoreparties.co/uploads/thumbnail_881707734_640_d6a3a43358.jpeg"
+  },
+  {
+    nameRU: "Постеры, сошедшие со стен",
+    duration: 32,
+    url: "https://api.nomoreparties.co/uploads/thumbnail_posters_came_from_the_walls_2009_001_posters_180fe1a19f.jpeg"
+  },
+  {
+    nameRU: "Soul Power",
+    duration: 92,
+    url: "https://api.nomoreparties.co/uploads/thumbnail_images_5bfcbf36e6.jpeg"
+  },
+  {
+    nameRU: "196 ударов в минуту",
+    duration: 60,
+    url: "https://api.nomoreparties.co/uploads/thumbnail_zagruzhennoe_1_fd5faff237.jpeg"
+  },
+  {
+    nameRU: "Баллада о Дженезисе и Леди Джей",
+    duration: 65,
+    url: "https://api.nomoreparties.co/uploads/thumbnail_ballad_of_genesis_and_lady_jaye_10c27afa96.jpeg"
+  },
+  {
+    nameRU: "Виллалобос",
+    duration: 110,
+    url: "https://api.nomoreparties.co/uploads/thumbnail_590x400_2eccd40a93.jpeg"
+  },
+]
