411cb7fdf6f664dabffbe6f58f82bc628972c465
Georgy Yadrihinskiy <y-georgy@yandex.ru>
2021-11-30 03:04:25 +0300

    refactor: обновление замоканных данных

--- movies-explorer-frontend/src/utils/initialCards.ts
+++ movies-explorer-frontend/src/utils/initialCards.ts
@@ -16,10 +16,10 @@ export const initialCards: ICard[] = [
   },
   {
     _id: "2",
-    nameRU: "Taqwacore: The Birth of Punk Islam",
+    nameRU: "Marsli",
     duration: 80,
     trailerLink: "https://www.youtube.com/watch?v=JMZ8DO9F4Mo",
-    url: "https://api.nomoreparties.co/uploads/taqwacore2_2f487d2e74.jpeg"
+    url: "https://www.kinogallery.com/images/the-martian2016/kinogallery.com-the-martian2016-0513164001444285815.jpg"
   },
   {
     _id: "3",
@@ -69,5 +69,61 @@ export const initialCards: ICard[] = [
     duration: 110,
     trailerLink: "https://www.kinopoisk.ru/film/586534/video/56500/",
     url: "https://api.nomoreparties.co/uploads/590x400_2eccd40a93.jpeg"
+  },
+  {
+    _id: "10",
+    nameRU: "Роллинг Стоунз» в изгнании",
+    duration: 61,
+    trailerLink: "https://www.youtube.com/watch?v=UXcqcdYABFw",
+    url: "https://api.nomoreparties.co/uploads/stones_in_exile_b2f1b8f4b7.jpeg"
+  },
+  {
+    _id: "11",
+    nameRU: "Marsli",
+    duration: 80,
+    trailerLink: "https://www.youtube.com/watch?v=JMZ8DO9F4Mo",
+    url: "https://www.kinogallery.com/images/the-martian2016/kinogallery.com-the-martian2016-0513164001444285815.jpg"
+  },
+  {
+    _id: "12",
+    nameRU: "Без обратного пути",
+    duration: 104,
+    trailerLink: "https://www.youtube.com/watch?v=6iYxdghpJZY",
+    url: "https://api.nomoreparties.co/uploads/blur_a43fcf463d.jpeg"
+  },
+  {
+    _id: "13",
+    nameRU: "Фавела на взрыве",
+    duration: 80,
+    trailerLink: "https://www.youtube.com/watch?v=Cugdwa7mndA",
+    url: "https://api.nomoreparties.co/uploads/881707734_640_d6a3a43358.jpeg"
+  },
+  {
+    _id: "14",
+    nameRU: "Постеры, сошедшие со стен",
+    duration: 32,
+    trailerLink: "https://www.youtube.com/watch?v=VFMU3crg0sM",
+    url: "https://api.nomoreparties.co/uploads/posters_came_from_the_walls_2009_001_posters_180fe1a19f.jpeg"
+  },
+  {
+    _id: "15",
+    nameRU: "Soul Power",
+    duration: 92,
+    trailerLink: "https://www.youtube.com/watch?v=8OprNgiOq-I",
+    url: "https://api.nomoreparties.co/uploads/images_5bfcbf36e6.jpeg"
+  },
+  {
+    _id: "16",
+    nameRU: "196 ударов в минуту",
+    duration: 60,
+    trailerLink: "https://www.youtube.com/watch?v=GsDRVpdgNJ4",
+    url: "https://api.nomoreparties.co/uploads/zagruzhennoe_1_fd5faff237.jpeg"
+  },
+  {
+    _id: "17",
+    nameRU: "Баллада о Дженезисе и Леди Джей",
+    duration: 65,
+    trailerLink: "https://www.youtube.com/watch?v=d8BX2FDrogo",
+    url: "https://api.nomoreparties.co/uploads/ballad_of_genesis_and_lady_jaye_10c27afa96.jpeg"
   },
 ]
