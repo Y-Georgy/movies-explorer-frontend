17a9f58eda5a372dfbf627ecaa6f920838319bf7
Georgy Yadrihinskiy <y-georgy@yandex.ru>
2021-11-30 13:28:57 +0300

    feat: функционал getTimeFromMins и handleBtnSave

--- movies-explorer-frontend/src/components/MoviesCard/MoviesCard.tsx
+++ movies-explorer-frontend/src/components/MoviesCard/MoviesCard.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './MoviesCard.css'
 // import { ICard } from '../../utils/initialCards';
@@ -13,12 +14,26 @@ interface Props {
 }
 
 const MoviesCard = ({ movieCard }: Props) => {
+  const [isSavedMovie, setIsSavedMovie] = useState<Boolean>(false)
+
+  function handleClickBtnSave() {
+    setIsSavedMovie(!isSavedMovie);
+  }
+
+  function getTimeFromMins() {
+    const hours = Math.floor(movieCard.duration/60);
+    const minutes = movieCard.duration % 60;
+    return hours + 'ч ' + minutes + 'м';
+  };
+
   return (
       <li className="movies-card">
-        <a href={movieCard.trailerLink} target="_blank"><img src={movieCard.url} alt="Обложка фильма" className="movies-card__image" /></a>
+        <a href={movieCard.trailerLink} target="_blank">
+          <img src={movieCard.url} alt="Обложка фильма" className="movies-card__image" />
+        </a>
         <p className="movies-card__name">{movieCard.nameRU}</p>
-        <p className="movies-card__duration">{movieCard.duration}</p>
-        <button className="movies-card__save movies-card__save_active" />
+        <p className="movies-card__duration">{getTimeFromMins()}</p>
+        <button className={`movies-card__save${isSavedMovie ? ' movies-card__save_active' : ''}`} onClick={handleClickBtnSave} />
       </li>
   )
 }
