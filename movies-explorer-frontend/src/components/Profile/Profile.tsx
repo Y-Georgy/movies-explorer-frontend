2e1ed847907c78c445f87daed8155d1caecb5600
Georgy Yadrihinskiy <y-georgy@yandex.ru>
2021-12-02 01:14:50 +0300

    feat: адаптивная верстка Profile

--- movies-explorer-frontend/src/components/Profile/Profile.tsx
+++ movies-explorer-frontend/src/components/Profile/Profile.tsx
@@ -1,6 +1,4 @@
 import './Profile.css'
-import { Link } from 'react-router-dom';
-import logo from '../../images/logo.svg'
 import Header from '../Header/Header';
 import NavPage from '../NavPage/NavPage';
 
@@ -8,27 +6,23 @@ function Profile() {
   return (
     <>
       <Header children={<NavPage />} bgcolor="grey"/>
-      <main className="login">
-        <Link to="/" className="login__link-logo">
-          <img src={logo} alt="Логотип Movies-explorer" className="logo" />
-        </Link>
-        <h1 className="login__title">Рады видеть!</h1>
-        <form className="form-user" name="form-login">
+      <main className="profile">
+        <h1 className="profile__title">Привет, Виталий!</h1>
+         <form className="form-profile" name="form-profile">
+          <label className="form-profile__label">
+            Имя
+            <input type="text" className="form-profile__input" name="name" value="Виталий" />
+            </label>
 
-          <label htmlFor="email" className="form-user__label">E-mail</label>
-          <input required id="email" type="email" className="form-user__input" name="email" />
-          <span className="form-user__error email-input-error"></span>
+          <label className="form-profile__label">
+            E-mail
+            <input type="email" className="form-profile__input" name="email" value="pochta@yandex.ru" />
+          </label>
 
-          <label htmlFor="password" className="form-user__label">Пароль</label>
-          <input required id="password" type="password" className="form-user__input" name="password" />
-          <span className="form-user__error password-input-error"></span>
-
-          <button type="submit" className="form-user__submit-button form-user__submit-button_margin_top">Войти</button>
+          <span className="form-profile__error">При обновлении профиля произошла ошибка.</span>
+          <button type="submit" className="form-profile__submit-button">Редактировать</button>
         </form>
-        <p className="login__text">
-          Ещё не зарегистрированы?
-          <Link to="/signup" className="login__link">Регистрация</Link>
-        </p>
+        <button type="button" className="profile__logout-button">Выйти из аккаунта</button>
       </main>
     </>
   )
