8273e4cb8f0812dfe77b44ef066227b5dd9797bd
Georgy Yadrihinskiy <y-georgy@yandex.ru>
2021-12-02 02:24:32 +0300

    feat: замена кнопки save на delete на странице saved-movies

--- movies-explorer-frontend/src/components/MoviesCard/MoviesCard.tsx
+++ movies-explorer-frontend/src/components/MoviesCard/MoviesCard.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { useLocation } from 'react-router-dom';
 import './MoviesCard.css'
 // import { ICard } from '../../utils/initialCards';
 
@@ -14,6 +15,7 @@ interface Props {
 
 const MoviesCard = ({ movieCard }: Props) => {
   const [isSavedMovie, setIsSavedMovie] = useState<Boolean>(false)
+  const { pathname } = useLocation();
 
   function handleClickBtnSave() {
     setIsSavedMovie(!isSavedMovie);
@@ -25,6 +27,10 @@ const MoviesCard = ({ movieCard }: Props) => {
     return hours + 'ч ' + minutes + 'м';
   };
 
+  function handleClickBtnDelete() {
+
+  }
+
   return (
       <li className="movies-card">
         <a href={movieCard.trailerLink} target="_blank" rel="noreferrer">
@@ -32,7 +38,8 @@ const MoviesCard = ({ movieCard }: Props) => {
         </a>
         <p className="movies-card__name">{movieCard.nameRU}</p>
         <p className="movies-card__duration">{getTimeFromMins()}</p>
-        <button className={`movies-card__save${isSavedMovie ? ' movies-card__save_active' : ''}`} onClick={handleClickBtnSave} />
+        {pathname === "/movies" && <button className={`movies-card__button movies-card__button_icon_save${isSavedMovie ? ' movies-card__button_icon_active' : ''}`} onClick={handleClickBtnSave} />}
+        {pathname === "/saved-movies" && <button className="movies-card__button movies-card__button_icon_delete" onClick={handleClickBtnDelete} />}
       </li>
   )
 }
