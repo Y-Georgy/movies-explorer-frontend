35d02f3e1ac6817caac4b754a84abe810e161867
Georgy Yadrihinskiy <y-georgy@yandex.ru>
2021-12-01 14:25:41 +0300

    feat: адаптивная верстка Register

--- movies-explorer-frontend/src/components/App/App.tsx
+++ movies-explorer-frontend/src/components/App/App.tsx
@@ -4,6 +4,7 @@ import Main from '../Main/Main';
 import Movies from '../Movies/Movies';
 import Footer from '../Footer/Footer';
 import NavAuth from '../NavAuth/NavAuth';
+import Register from '../Register/Register';
 import './App.css';
 import { Route, Routes } from 'react-router-dom'
 
@@ -20,6 +21,7 @@ function App() {
             <Footer />
           </>
         } />
+        <Route path="/signup" element={<Register />} />
       </Routes>
     </>
   );
---
+++ movies-explorer-frontend/src/components/Register/Register.tsx
@@ -0,0 +1,37 @@
+import './Register.css'
+import Header from '../Header/Header';
+import NavPage from '../NavPage/NavPage';
+import { Link } from 'react-router-dom';
+import logo from '../../images/logo.svg'
+
+function Register() {
+  return (
+      <main className="register">
+        <Link to="/" className="register__link-logo">
+          <img src={logo} alt="Логотип Movies-explorer" className="register__logo" />
+        </Link>
+        <h1 className="register__title">Добро пожаловать!</h1>
+        <form className="form-user" name="form-register">
+          <label htmlFor="name" className="form-user__label">Имя</label>
+          <input required id="name" type="text" className="form-user__input" name="name" />
+          <span className="form-user__error name-input-error"></span>
+
+          <label htmlFor="email" className="form-user__label">E-mail</label>
+          <input required id="email" type="email" className="form-user__input" name="email" />
+          <span className="form-user__error email-input-error"></span>
+
+          <label htmlFor="password" className="form-user__label">Пароль</label>
+          <input required id="password" type="password" className="form-user__input" name="password" />
+          <span className="form-user__error password-input-error"></span>
+
+          <button type="submit" className="form-user__submit-button">Зарегистрироваться</button>
+        </form>
+        <p className="register__text">
+          Уже зарегистрированы?
+          <Link to="/signin" className="register__link">Войти</Link>
+        </p>
+      </main>
+  )
+}
+
+export default Register;
