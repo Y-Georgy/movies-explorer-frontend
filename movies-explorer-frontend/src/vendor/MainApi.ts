8a11b84d8b1bbff4a34d3cfea7c5fc527ddff258
Georgy Yadrihinskiy <y-georgy@yandex.ru>
2021-12-06 18:58:43 +0300

    feat: mainApi

---
+++ movies-explorer-frontend/src/vendor/MainApi.ts
@@ -0,0 +1,75 @@
+import React from "react";
+
+interface IOptionMainApi {
+  baseUrl: string,
+  headers: {
+    'Content-Type': string
+  }
+}
+
+class MainApi {
+  _baseUrl: string;
+  _headers: { 'Content-Type': string; };
+
+  constructor({ baseUrl, headers }: IOptionMainApi) {
+    this._baseUrl = baseUrl
+    this._headers = headers
+  }
+
+  // обработчик ответа
+  _handleResponse(res: { ok: boolean; json: () => any; status: any; }) {
+    if (res.ok) {
+      return res.json()
+    }
+    return Promise.reject(`Произошла ошибка: ${res.status}`)
+  }
+
+  getMovies() {
+    return fetch(`${this._baseUrl}/movies`, {
+      method: 'GET',
+      credentials: 'include',
+      headers: this._headers,
+    }).then(this._handleResponse)
+  }
+
+  postMovies(dataMovie: any) {
+    return fetch(`${this._baseUrl}/movies`, {
+      method: 'POST',
+      credentials: 'include',
+      headers: this._headers,
+      body: JSON.stringify(dataMovie),
+    }).then(this._handleResponse)
+  }
+
+  deleteMovie(movieId: string) {
+    return fetch(`${this._baseUrl}/movies/${movieId}`, {
+      method: 'DELETE',
+      credentials: 'include',
+      headers: this._headers,
+    }).then(this._handleResponse)
+  }
+
+  getProfile() {
+    return fetch(`${this._baseUrl}/users/me`, {
+      method: 'GET',
+      credentials: 'include',
+      headers: this._headers,
+    }).then(this._handleResponse)
+  }
+
+  updateProfile(user: any) {
+    return fetch(`${this._baseUrl}/users/me/`, {
+      method: 'PATCH',
+      credentials: 'include',
+      headers: this._headers,
+      body: JSON.stringify(user), // передавать name и email
+    }).then(this._handleResponse)
+  }
+}
+
+export const mainApi = new MainApi({
+  baseUrl: "https://api.movie.nomoredomains.rocks",
+  headers: {
+    'Content-Type': 'application/json',
+  },
+})
