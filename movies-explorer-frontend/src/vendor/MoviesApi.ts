371c18c3d9321d2474bf19bba9ce7b5296d54a53
Georgy Yadrihinskiy <y-georgy@yandex.ru>
2021-12-06 19:28:53 +0300

    feat: MoviesApi

---
+++ movies-explorer-frontend/src/vendor/MoviesApi.ts
@@ -0,0 +1,39 @@
+import React from "react";
+
+interface IOptionMoviesApi {
+  baseUrl: string,
+  headers: { 'Content-Type': string }
+}
+
+class MoviesApi {
+  _baseUrl: string;
+  _headers: { 'Content-Type': string; };
+
+  constructor({ baseUrl, headers }: IOptionMoviesApi) {
+    this._baseUrl = baseUrl
+    this._headers = headers
+  }
+
+  // обработчик ответа
+  _handleResponse(res: { ok: boolean; json: () => any; status: any; }) {
+    if (res.ok) {
+      return res.json()
+    }
+    return Promise.reject(`Произошла ошибка: ${res.status}`)
+  }
+
+  getMovies() {
+    return fetch(`${this._baseUrl}`, {
+      method: 'GET',
+      credentials: 'include',
+      headers: this._headers,
+    }).then(this._handleResponse)
+  }
+}
+
+export const mainApi = new MoviesApi({
+  baseUrl: "https://api.nomoreparties.co/beatfilm-movies",
+  headers: {
+    'Content-Type': 'application/json',
+  },
+})
