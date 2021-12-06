511dd90034c9cf98479c8b4da53d52b355a83430
Georgy Yadrihinskiy <y-georgy@yandex.ru>
2021-12-06 12:44:08 +0300

    feat: встроил Preloader и сообщение isContent

--- movies-explorer-frontend/src/components/MoviesCardList/MoviesCardList.tsx
+++ movies-explorer-frontend/src/components/MoviesCardList/MoviesCardList.tsx
@@ -2,55 +2,50 @@ import './MoviesCardList.css'
 import MoviesCard from '../MoviesCard/MoviesCard';
 import { initialCards } from '../../utils/initialCards';
 import { ICard } from '../../utils/initialCards';
-import React, { useState } from 'react';
-// import Preloader from '../Preloader/Preloader';
+import React, { useEffect, useState } from 'react';
+import Preloader from '../Preloader/Preloader';
 
 function MoviesCardList() {
-// eslint-disable-next-line
-const [movies, setMovies] = useState<ICard[]>(initialCards)
-const [quantityRenderMovies, setQuantityRenderMovies] = useState<number>(6)
+  // eslint-disable-next-line
+  const [movies, setMovies] = useState<ICard[]>(initialCards)
+  const [quantityRenderMovies, setQuantityRenderMovies] = useState<number>(6)
+  const [renderMovies, setRenderMovies] = useState<React.ReactNode[]>([])
+  const [isContent, setIsContent] = useState<boolean>(true);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
-// useEffect(() => {
-//   if (allMovies.length >= isAnotherMovies) {
-//     setIsAnotherMovies(true);
-//   }
-// },[isAnotherMovies])
+  // времменный эффект для тестирования верстки
+  useEffect(() => {
+      setIsLoading(true);
+      function testLoading() {
+        let content = [];
+        for (let i = 0; i < quantityRenderMovies && i < movies.length; i++) {
+          content.push(<MoviesCard
+              key={movies[i]._id}
+              movieCard={movies[i]}
+            />);
+        }
+        if (content.length === 0) {
+          setIsContent(false);
+        } else {
+        setIsContent(true);
+        }
+        setIsLoading(false);
+        setRenderMovies(content);
+      };
+      setTimeout(testLoading, 2000); // убрать по готовности API
+  }, [quantityRenderMovies]);
 
-// {movies.map((movie) => (
-//   <MoviesCard
-//     key={movie._id}
-//     movieCard={movie}
-//   />
-// ))}
-
-// {for (let i = 0; i <= ${quantityRenderMovies}; i++) {
-//   <MoviesCard
-//   key={movies[i]._id}
-//   movieCard={movies[i]}
-//   />
-// }}
-
-function renderCards() {
-  let content = [];
-  for (let i = 0; i < quantityRenderMovies && i < movies.length; i++) {
-    content.push(<MoviesCard
-        key={movies[i]._id}
-        movieCard={movies[i]}
-      />);
+  function handleClickBtnAnother() {
+    setQuantityRenderMovies(quantityRenderMovies + 6);
   }
-  return content;
-};
-
-function handleClickBtnAnother() {
-  setQuantityRenderMovies(quantityRenderMovies + 6);
-}
 
   return (
-      <section className="movies-card-list">
-        <ul className="movies-card-list__container">{renderCards()}</ul>
-        {/* <Preloader /> */}
-        {movies.length > quantityRenderMovies && <button  className="movies-card-list__button-another" onClick={handleClickBtnAnother}>Ещё</button>}
-      </section>
+    <section className="movies-card-list">
+      <ul className="movies-card-list__container">{renderMovies}</ul>
+      {isLoading && <Preloader />}
+      {!isContent && <span className="movies-card-list__content-not-found">Ни чего не найдено</span>}
+      {(movies.length > quantityRenderMovies && !isLoading) && <button  className="movies-card-list__button-another" onClick={handleClickBtnAnother}>Ещё</button>}
+    </section>
   )
 }
 
