548e1f7d928b65585ebf64673401ede20b5d4e5c
Georgy Yadrihinskiy <y-georgy@yandex.ru>
2021-12-09 00:09:04 +0300

    feat: рендеринг карточек по устройствам

--- movies-explorer-frontend/src/components/MoviesCardList/MoviesCardList.tsx
+++ movies-explorer-frontend/src/components/MoviesCardList/MoviesCardList.tsx
@@ -1,6 +1,5 @@
 import './MoviesCardList.css'
 import MoviesCard from '../MoviesCard/MoviesCard';
-import { initialCards } from '../../utils/initialCards';
 import { ICard } from '../../utils/initialCards';
 import React, { useEffect, useState } from 'react';
 import Preloader from '../Preloader/Preloader';
@@ -12,8 +11,6 @@ interface Props {
 }
 
 function MoviesCardList({ movies, isLoadingMovies, massageSearchMovies }: Props) {
-  const [quantityRenderMovies, setQuantityRenderMovies] = useState<number>(6)
-  const [quantityRenderMoviesAnother, setQuantityRenderMoviesAnother] = useState<number>(6)
   const [renderMovies, setRenderMovies] = useState<ICard[]>([])
 
   function checkClientWindow() {
@@ -21,7 +18,7 @@ function MoviesCardList({ movies, isLoadingMovies, massageSearchMovies }: Props)
     if (windowWidth >= 1280) return 'l'
     else if (windowWidth < 768) return 's'
     else return 'm'
-    }
+  }
 
   function getQuantityToFirstLoad(screen: 's' | 'm' | 'l') {
     if (screen === 's') return 5
@@ -33,15 +30,6 @@ function MoviesCardList({ movies, isLoadingMovies, massageSearchMovies }: Props)
     if (screen === 'l') return 3
     else return 2
   }
-
-  // RESIZE ведь не нужен???
-  // useEffect(() => {
-  //   let timeOutCheckClientWindow: any;
-  //   window.addEventListener('resize', () => {
-  //     clearTimeout(timeOutCheckClientWindow);
-  //     timeOutCheckClientWindow = setTimeout(checkClientWindow, 1000)
-  //   })
-  // }, [])
 
   function handleClickBtnAnother() {
     const quantityRenderedMovies = renderMovies.length;
@@ -62,16 +50,16 @@ function MoviesCardList({ movies, isLoadingMovies, massageSearchMovies }: Props)
   return (
     <section className="movies-card-list">
       <ul className="movies-card-list__container">
-        {renderMovies.length !== 0 && renderMovies.map((movie) => {
-          return <MoviesCard
+        {renderMovies.length !== 0 && renderMovies.map((movie) => (
+          <MoviesCard
             key={movie.id}
             movieCard={movie}
           />
-        })}
+        ))}
       </ul>
       {isLoadingMovies && <Preloader />}
       {massageSearchMovies && <span className="movies-card-list__content-not-found">{massageSearchMovies}</span>}
-      {(movies.length > quantityRenderMovies && !isLoadingMovies) && <button  className="movies-card-list__button-another" onClick={handleClickBtnAnother}>Ещё</button>}
+      {movies.length > renderMovies.length && <button  className="movies-card-list__button-another" onClick={handleClickBtnAnother}>Ещё</button>}
     </section>
   )
 }
