e296d747906ac5a6f5770ae79d3424b62924e49d
Georgy Yadrihinskiy <y-georgy@yandex.ru>
2021-12-09 11:45:34 +0300

    feat: валидация компонента Register

--- movies-explorer-frontend/src/components/Register/Register.tsx
+++ movies-explorer-frontend/src/components/Register/Register.tsx
@@ -1,8 +1,28 @@
 import './Register.css'
 import { Link } from 'react-router-dom';
 import logo from '../../images/logo.svg'
+import { UseFormWithValidation } from '../UseFormWithValidation';
+import { useEffect } from 'react';
+
+interface IForm {
+  values: {
+    name?: string,
+    email?: string,
+    password?: string,
+  },
+  errors: {
+    name?: string,
+    email?: string,
+    password?: string,
+  } ,
+  isValid: boolean,
+  resetForm: () => void,
+  handleChange: (evt: any) => void
+}
 
 function Register() {
+  const { values, handleChange, errors, isValid, resetForm }: IForm = UseFormWithValidation()
+
   return (
       <main className="register">
         <Link to="/" className="register__link-logo">
@@ -11,18 +31,51 @@ function Register() {
         <h1 className="register__title">Добро пожаловать!</h1>
         <form className="form-user" name="form-register">
           <label htmlFor="name" className="form-user__label">Имя</label>
-          <input required id="name" type="text" className="form-user__input" name="name" minLength={2} maxLength={30} placeholder="Введите имя" />
-          <span className="form-user__error name-input-error"></span>
+          <input
+            required
+            id="name"
+            type="text"
+            className="form-user__input"
+            name="name"
+            minLength={2}
+            maxLength={30}
+            placeholder="Введите имя"
+            onChange={handleChange}
+          />
+          <span className="form-user__error name-input-error">{errors.name !== undefined && errors.name}</span>
 
           <label htmlFor="email" className="form-user__label">E-mail</label>
-          <input required id="email" type="email" className="form-user__input" name="email" placeholder="Введите e-mail" />
-          <span className="form-user__error email-input-error"></span>
+          <input
+            required
+            id="email"
+            type="email"
+            className="form-user__input"
+            name="email"
+            placeholder="Введите e-mail"
+            onChange={handleChange}
+          />
+          <span className="form-user__error email-input-error">{errors.email !== undefined && errors.email}</span>
 
           <label htmlFor="password" className="form-user__label">Пароль</label>
-          <input required id="password" type="password" className="form-user__input" name="password" minLength={8} placeholder="Введите пароль" />
-          <span className="form-user__error password-input-error"></span>
+          <input
+            required
+            id="password"
+            type="password"
+            className="form-user__input"
+            name="password"
+            minLength={8}
+            placeholder="Введите пароль"
+            onChange={handleChange}
+            />
+          <span className="form-user__error password-input-error">{errors.password !== undefined && errors.password}</span>
 
-          <button type="submit" className="form-user__submit-button">Зарегистрироваться</button>
+          <button
+            type="submit"
+            className="form-user__submit-button"
+            disabled={Object.values(errors).join('') !== ''}
+          >
+            Зарегистрироваться
+          </button>
         </form>
         <p className="register__text">
           Уже зарегистрированы?
