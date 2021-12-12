38f1373fe91f85f4151e263e2fd61b243383b145
Georgy Yadrihinskiy <y-georgy@yandex.ru>
2021-12-12 23:39:26 +0300

    feat: валидация login

--- movies-explorer-frontend/src/components/Login/Login.tsx
+++ movies-explorer-frontend/src/components/Login/Login.tsx
@@ -1,25 +1,53 @@
 import './Login.css'
 import { Link } from 'react-router-dom';
-import logo from '../../images/logo.svg'
+import logo from '../../images/logo.svg';
+import { UserFormValidator } from '../UserFormValidator/UserFormValidator';
 
 function Login() {
+  const { values, validators, handleChange, isValidForm } = UserFormValidator()
+
   return (
       <main className="login">
         <Link to="/" className="login__link-logo">
           <img src={logo} alt="Логотип Movies-explorer" className="logo" />
         </Link>
         <h1 className="login__title">Рады видеть!</h1>
-        <form className="form-user" name="form-login">
+        <form className="form-user" name="form-login" noValidate>
 
           <label htmlFor="email" className="form-user__label">E-mail</label>
-          <input required id="email" type="email" className="form-user__input" name="email" placeholder="Введите имя" />
-          <span className="form-user__error email-input-error"></span>
+          <input
+            id="email"
+            type="email"
+            className="form-user__input"
+            name="email"
+            placeholder="Введите имя"
+            onChange={handleChange}
+          />
+          <span className="form-user__error email-input-error">
+            {!validators.isValidEmail && 'Введён не корректный e-mail'}
+          </span>
 
           <label htmlFor="password" className="form-user__label">Пароль</label>
-          <input required id="password" type="password" className="form-user__input" name="password"  minLength={8} placeholder="Введите пароль" />
-          <span className="form-user__error password-input-error"></span>
+          <input
+            id="password"
+            type="password"
+            className="form-user__input"
+            name="password"
+            minLength={8}
+            placeholder="Введите пароль"
+            onChange={handleChange}
+          />
+          <span className="form-user__error password-input-error">
+            {!validators.isValidPassword && 'Пароль должен содержать минимум 8 знаков'}
+          </span>
 
-          <button type="submit" className="form-user__submit-button form-user__submit-button_margin_top" >Войти</button>
+          <button
+            type="submit"
+            className="form-user__submit-button form-user__submit-button_margin_top"
+            disabled={!isValidForm() || values.email === '' || values.password === ''}
+          >
+            Войти
+          </button>
         </form>
         <p className="login__text">
           Ещё не зарегистрированы?
