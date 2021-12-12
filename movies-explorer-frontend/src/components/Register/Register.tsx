e4f08f2beae0fe206cf861ff232245ba49b25469
Georgy Yadrihinskiy <y-georgy@yandex.ru>
2021-12-12 23:39:08 +0300

    feat: функционал формы register

--- movies-explorer-frontend/src/components/Register/Register.tsx
+++ movies-explorer-frontend/src/components/Register/Register.tsx
@@ -1,73 +1,100 @@
 import './Register.css'
+import { useNavigate } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import logo from '../../images/logo.svg'
 import { UserFormValidator } from '../UserFormValidator/UserFormValidator';
+import { mainApi } from '../../vendor/MainApi';
+import { useEffect, useState } from 'react';
 
 function Register() {
+  const navigate = useNavigate();
   const { values, validators, handleChange, isValidForm } = UserFormValidator()
+  const [errorRegister, setErrorRegister] = useState<string>('')
 
-   return (
-      <main className="register">
-        <Link to="/" className="register__link-logo">
-          <img src={logo} alt="Логотип Movies-explorer" className="logo" />
-        </Link>
-        <h1 className="register__title">Добро пожаловать!</h1>
-        <form className="form-user" name="form-register" noValidate>
-          <label htmlFor="name" className="form-user__label">Имя</label>
-          <input
-            required
-            id="name"
-            type="text"
-            className="form-user__input"
-            name="name"
-            placeholder="Введите имя"
-            onChange={handleChange}
+  function handleSubmit(e: any) {
+    e.preventDefault()
+    mainApi.register(values)
+      .then((res) => {
+        mainApi.login({ email: values.email, password: values.password })
+          .then(res => {
+            navigate('/movies');
+          })
+          .catch(err => {setErrorRegister(err)});
+      })
+      .catch((err) => {
+        setErrorRegister(err);
+      })
+  }
+
+  useEffect(() => {
+    setErrorRegister('')
+  }, [values])
+
+  return (
+    <main className="register">
+      <Link to="/" className="register__link-logo">
+        <img src={logo} alt="Логотип Movies-explorer" className="logo" />
+      </Link>
+      <h1 className="register__title">Добро пожаловать!</h1>
+      <form className="form-user" name="form-register" noValidate onSubmit={handleSubmit}>
+        <label htmlFor="name" className="form-user__label">Имя</label>
+        <input
+          required
+          id="name"
+          type="text"
+          className="form-user__input"
+          name="name"
+          placeholder="Введите имя"
+          onChange={handleChange}
+        />
+        <span className="form-user__error name-input-error">
+          {!validators.isValidName && 'Имя может содержать только латиницу, кириллицу, пробел или дефис'}
+        </span>
+
+        <label htmlFor="email" className="form-user__label">E-mail</label>
+        <input
+          required
+          id="email"
+          type="email"
+          className="form-user__input"
+          name="email"
+          placeholder="Введите e-mail"
+          onChange={handleChange}
+        />
+        <span className="form-user__error email-input-error">
+          {!validators.isValidEmail && 'Введён не корректный e-mail'}
+        </span>
+
+        <label htmlFor="password" className="form-user__label">Пароль</label>
+        <input
+          required
+          id="password"
+          type="password"
+          className="form-user__input"
+          name="password"
+          placeholder="Введите пароль"
+          onChange={handleChange}
           />
-          <span className="form-user__error name-input-error">
-            {!validators.isValidName && 'Имя может содержать только латиницу, кириллицу, пробел или дефис'}
-          </span>
+        <span className="form-user__error password-input-error">
+          {!validators.isValidPassword && 'Пароль должен содержать минимум 8 знаков'}
+        </span>
 
-          <label htmlFor="email" className="form-user__label">E-mail</label>
-          <input
-            required
-            id="email"
-            type="email"
-            className="form-user__input"
-            name="email"
-            placeholder="Введите e-mail"
-            onChange={handleChange}
-          />
-          <span className="form-user__error email-input-error">
-            {!validators.isValidEmail && 'Введён не корректный e-mail'}
-          </span>
-
-          <label htmlFor="password" className="form-user__label">Пароль</label>
-          <input
-            required
-            id="password"
-            type="password"
-            className="form-user__input"
-            name="password"
-            placeholder="Введите пароль"
-            onChange={handleChange}
-            />
-          <span className="form-user__error password-input-error">
-            {!validators.isValidPassword && 'Пароль должен содержать минимум 8 знаков'}
-          </span>
-
-          <button
-            type="submit"
-            className="form-user__submit-button"
-            disabled={!isValidForm() || values.name === '' || values.email === '' || values.password === ''}
-          >
-            Зарегистрироваться
-          </button>
-        </form>
-        <p className="register__text">
-          Уже зарегистрированы?
-          <Link to="/signin" className="register__link">Войти</Link>
-        </p>
-      </main>
+        <span className="form-user__error form-user__submit-error">
+          {errorRegister}
+        </span>
+        <button
+          type="submit"
+          className="form-user__submit-button"
+          disabled={!isValidForm() || values.name === '' || values.email === '' || values.password === ''}
+        >
+          Зарегистрироваться
+        </button>
+      </form>
+      <p className="register__text">
+        Уже зарегистрированы?
+        <Link to="/signin" className="register__link">Войти</Link>
+      </p>
+    </main>
   )
 }
 
