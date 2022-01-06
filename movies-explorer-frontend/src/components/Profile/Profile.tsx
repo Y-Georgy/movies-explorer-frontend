2a6942221420b8cbe6afbeff83d0de248a8d01be
Georgy Yadrihinskiy <y-georgy@yandex.ru>
2022-01-06 23:32:55 +0300

    fix: signout

--- movies-explorer-frontend/src/components/Profile/Profile.tsx
+++ movies-explorer-frontend/src/components/Profile/Profile.tsx
@@ -1,67 +1,83 @@
-import './Profile.css'
-import React, { useEffect, useState } from 'react';
-import Header from '../Header/Header';
-import Navigation from '../Navigation/Navigation';
-import { mainApi } from '../../utils/MainApi';
-import { CurrentUserContext } from '../../contexts/CurrentUserContext';
-import { UserFormValidator } from '../UserFormValidator/UserFormValidator';
-import { useNavigate } from 'react-router-dom';
+import "./Profile.css";
+import React, { useEffect, useState } from "react";
+import Header from "../Header/Header";
+import Navigation from "../Navigation/Navigation";
+import { mainApi } from "../../utils/MainApi";
+import { CurrentUserContext } from "../../contexts/CurrentUserContext";
+import { UserFormValidator } from "../UserFormValidator/UserFormValidator";
+import { useNavigate } from "react-router-dom";
 
 interface Props {
-  setLoggedIn: (value: boolean) => void
+  setLoggedIn: (value: boolean) => void;
 }
 
-function Profile( {setLoggedIn }: Props ) {
-  const { currentUser, setCurrentUser } = React.useContext(CurrentUserContext)
-  const { values, setValues, validators, handleChange, isValidForm } = UserFormValidator()
-  const [errorSubmitEditPrifile, setErrorSubmitEditPrifile] = useState<String>('')
-  const [seccessMessageSubmit, setSeccessMessageSubmit] = useState<String>('')
-  const [isFormDisabled, setIsFormDisabled] = useState<boolean>(false)
+function Profile({ setLoggedIn }: Props) {
+  const { currentUser, setCurrentUser } = React.useContext(CurrentUserContext);
+  const { values, setValues, validators, handleChange, isValidForm } =
+    UserFormValidator();
+  const [errorSubmitEditPrifile, setErrorSubmitEditPrifile] =
+    useState<String>("");
+  const [seccessMessageSubmit, setSeccessMessageSubmit] = useState<String>("");
+  const [isFormDisabled, setIsFormDisabled] = useState<boolean>(false);
   const navigate = useNavigate();
 
   useEffect(() => {
     if (currentUser.name && currentUser.email) {
-      setValues({ name: currentUser.name, email: currentUser.email, password: '' })
+      setValues({
+        name: currentUser.name,
+        email: currentUser.email,
+        password: "",
+      });
     }
-  }, [currentUser, setValues])
+  }, [currentUser, setValues]);
 
   function handleSubmitEditProfile(evt: any) {
     evt.preventDefault();
-    setIsFormDisabled(true)
-    setErrorSubmitEditPrifile('')
+    setIsFormDisabled(true);
+    setErrorSubmitEditPrifile("");
     const userData = {
       name: values.name,
-      email: values.email
-    }
-    mainApi.updateProfile( userData )
-      .then(res => {
-        setCurrentUser(res.data)
-        setSeccessMessageSubmit('Профиль успешно изменён')
-        setTimeout(() => {setSeccessMessageSubmit('')}, 3000)
+      email: values.email,
+    };
+    mainApi
+      .updateProfile(userData)
+      .then((res) => {
+        setCurrentUser(res.data);
+        setSeccessMessageSubmit("Профиль успешно изменён");
+        setTimeout(() => {
+          setSeccessMessageSubmit("");
+        }, 3000);
       })
-      .catch(err => {
-        setErrorSubmitEditPrifile(err)
+      .catch((err) => {
+        setErrorSubmitEditPrifile(err);
       })
-      .finally(() => setIsFormDisabled(false))
+      .finally(() => setIsFormDisabled(false));
   }
 
   function handleSignOut() {
-    mainApi.signOut()
-      .then(res => {
-        localStorage.removeItem('movies')
-        setLoggedIn(false)
-        setCurrentUser({})
-        navigate('/')
+    mainApi
+      .signOut()
+      .then((res) => {
+        setLoggedIn(false);
+        setCurrentUser({});
+        localStorage.removeItem("allMovies");
+        localStorage.removeItem("searchData");
+        navigate("/");
       })
-      .catch(console.log)
+      .catch(console.log);
   }
   if (currentUser) {
     return (
       <>
-        <Header children={<Navigation />} bgcolor="grey"/>
+        <Header children={<Navigation />} bgcolor="grey" />
         <main className="profile">
           <h1 className="profile__title">Привет, {currentUser.name}!</h1>
-          <form className="form-profile" name="form-profile" onSubmit={handleSubmitEditProfile} noValidate>
+          <form
+            className="form-profile"
+            name="form-profile"
+            onSubmit={handleSubmitEditProfile}
+            noValidate
+          >
             <label className="form-profile__label">
               Имя
               <input
@@ -88,29 +104,46 @@ function Profile( {setLoggedIn }: Props ) {
               />
             </label>
             <span className="form-profile__input-error">
-              {!validators.isValidNameLength && 'Минимальная длина имени 2 знака, максимальная 30 знаков'}
+              {!validators.isValidNameLength &&
+                "Минимальная длина имени 2 знака, максимальная 30 знаков"}
             </span>
             <span className="form-profile__input-error">
-              {!validators.isValidName && 'Имя может содержать только латиницу, кириллицу, пробел или дефис'}
+              {!validators.isValidName &&
+                "Имя может содержать только латиницу, кириллицу, пробел или дефис"}
             </span>
             <span className="form-profile__input-error">
-              {!validators.isValidEmail && 'Введён не корректный e-mail'}
+              {!validators.isValidEmail && "Введён не корректный e-mail"}
             </span>
 
-            <span className="form-profile__error">{errorSubmitEditPrifile && errorSubmitEditPrifile}</span>
-            <span className="form-profile__success-message">{seccessMessageSubmit && seccessMessageSubmit}</span>
+            <span className="form-profile__error">
+              {errorSubmitEditPrifile && errorSubmitEditPrifile}
+            </span>
+            <span className="form-profile__success-message">
+              {seccessMessageSubmit && seccessMessageSubmit}
+            </span>
             <button
               type="submit"
               className="form-profile__submit-button"
-              disabled={!isValidForm(['name', 'email']) || (values.name === currentUser.name && values.email === currentUser.email ) || isFormDisabled}
+              disabled={
+                !isValidForm(["name", "email"]) ||
+                (values.name === currentUser.name &&
+                  values.email === currentUser.email) ||
+                isFormDisabled
+              }
             >
               Сохранить
             </button>
           </form>
-          <button type="button" className="profile__logout-button" onClick={handleSignOut}>Выйти из аккаунта</button>
+          <button
+            type="button"
+            className="profile__logout-button"
+            onClick={handleSignOut}
+          >
+            Выйти из аккаунта
+          </button>
         </main>
       </>
-    )
+    );
   }
 }
 
